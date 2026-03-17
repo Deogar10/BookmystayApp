4dcc01c884809974e4893ab64304953b51b1f304
raghuvardhanmolli <raghuvardhanmolli@gmail.com>
2026-03-17 11:28:45 +0530

    completed uc1

---
+++ app/src/BookMyStayApp.java
@@ -0,0 +1,20 @@
+public class BookMyStayApp {
+
+    /**
+     * Main method - Entry point of the Java application.
+     * The JVM invokes this method when the program starts.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Display application welcome message
+        System.out.println("==================================");
+        System.out.println(" Welcome to Book My Stay ");
+        System.out.println(" Hotel Booking System v1.0 ");
+        System.out.println("==================================");
+        System.out.println("Application Started Successfully!");
+        System.out.println("Thank you for using Book My Stay. ");
+
+    }
+}
