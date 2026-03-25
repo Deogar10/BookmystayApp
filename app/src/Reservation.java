35339c651b03cfd7e1c15cef5fe9092abcee3195
Deogar10 <deoantony8284@gmail.com>
2026-03-25 10:52:42 +0530

    uc5 completed

---
+++ app/src/Reservation.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+// Reservation Class
+class Reservation {
+    private String guestName;
+    private String roomType;
+
+    public Reservation(String guestName, String roomType) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+}
+
+// Booking Queue
+class BookingRequestQueue {
+
+    private Queue<Reservation> queue;
+
+    public BookingRequestQueue() {
+        queue = new LinkedList<>();
+    }
+
+    // Add request
+    public void addRequest(Reservation r) {
+        queue.offer(r);
+    }
+
+    // Process requests (FIFO)
+    public void processRequests() {
+        System.out.println("Booking Request Queue");
+
+        while (!queue.isEmpty()) {
+            Reservation r = queue.poll(); // FIFO removal
+
+            System.out.println(
+                    "Processing booking for Guest: " +
+                            r.getGuestName() +
+                            ", Room Type: " +
+                            r.getRoomType()
+            );
+        }
+    }
+}
